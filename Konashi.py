ac589bd8b160fa0e14c5bd640b104a043d0f0275
Sebastian Miller <sebastian@ux-xu.com>
2021-03-21 19:29:23 +0900

    Added support for konashi builtin sensors and controls

--- Konashi.py
+++ Konashi.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import asyncio
 import logging
+import struct
 from ctypes import *
 from typing import *
 
@@ -46,6 +47,15 @@ KONASHI_UUID_I2C_DATA_IN = "064d0308-8251-49d9-b6f3-f7ba35e5d0a1"
 KONASHI_UUID_UART_DATA_IN = "064d0309-8251-49d9-b6f3-f7ba35e5d0a1"
 KONASHI_UUID_UART_DATA_SEND_DONE = "064d030a-8251-49d9-b6f3-f7ba35e5d0a1"
 KONASHI_UUID_SPI_DATA_IN = "064d030b-8251-49d9-b6f3-f7ba35e5d0a1"
+
+KONASHI_UUID_BUILTIN = "064d0400-8251-49d9-b6f3-f7ba35e5d0a1"
+KONASHI_UUID_BUILTIN_TEMPERATURE = "00002a6e-0000-1000-8000-00805f9b34fb"
+KONASHI_UUID_BUILTIN_HUMIDITY = "00002a6f-0000-1000-8000-00805f9b34fb"
+KONASHI_UUID_BUILTIN_PRESSURE = "00002a6d-0000-1000-8000-00805f9b34fb"
+KONASHI_UUID_BUILTIN_PRESENCE = "00002ae2-0000-1000-8000-00805f9b34fb"
+KONASHI_UUID_BUILTIN_ACCELGYRO = "064d0401-8251-49d9-b6f3-f7ba35e5d0a1"
+KONASHI_UUID_BUILTIN_RGB_SET = "064d0402-8251-49d9-b6f3-f7ba35e5d0a1"
+KONASHI_UUID_BUILTIN_RGB_GET = "064d0403-8251-49d9-b6f3-f7ba35e5d0a1"
 
 
 KONASHI_GPIO_COUNT = 8
@@ -188,6 +198,12 @@ class Konashi:
         self._gpio_output = _KonashiGpioPinsControl()
         self._gpio_input = _KonashiGpioPinsControl()
         self._gpio_input_cb = None
+        self._builtin_temperature_cb = None
+        self._builtin_humidity_cb = None
+        self._builtin_pressure_cb = None
+        self._builtin_presence_cb = None
+        self._builtin_accelgyro_cb = None
+        self._builtin_rgb_transition_end_cb = None
 
     def __str__(self):
         return f'Konashi {self._name} ({"Unknown" if self._ble_dev is None else self._ble_dev.address})'
@@ -309,6 +325,15 @@ class Konashi:
             await self._ble_client.start_notify(KONASHI_UUID_UART_DATA_SEND_DONE, self._ntf_cb_uart_data_send_done)
             await self._ble_client.start_notify(KONASHI_UUID_SPI_DATA_IN, self._ntf_cb_spi_data_in)
 
+            has_builtin = False
+            srvcs = await self._ble_client.get_services()
+            for s in srvcs:
+                if s.uuid == KONASHI_UUID_BUILTIN:
+                    has_builtin = True
+            if has_builtin:
+                print("Konashi has built-in sensors")
+                await self._ble_client.start_notify(KONASHI_UUID_BUILTIN_RGB_GET, self._ntf_cb_builtin_rgb_get)
+
     async def disconnect(self) -> None:
         if self._ble_client is not None:
             await self._ble_client.disconnect()
@@ -357,6 +382,46 @@ class Konashi:
     def _ntf_cb_spi_data_in(self, sender, data):
         pass
 
+    def _ntf_cb_builtin_temperature(self, sender, data):
+        d = struct.unpack("<h", data)
+        temp = d[0]
+        temp /= 100
+        if self._builtin_temperature_cb is not None:
+            self._builtin_temperature_cb(temp)
+    def _ntf_cb_builtin_humidity(self, sender, data):
+        d = struct.unpack("<h", data)
+        hum = d[0]
+        hum /= 100
+        if self._builtin_humidity_cb is not None:
+            self._builtin_humidity_cb(hum)
+    def _ntf_cb_builtin_pressure(self, sender, data):
+        d = struct.unpack("<i", data)
+        press = d[0]
+        press /= 1000
+        if self._builtin_pressure_cb is not None:
+            self._builtin_pressure_cb(press)
+    def _ntf_cb_builtin_presence(self, sender, data):
+        d = struct.unpack("<?", data)
+        pres = d[0]
+        if self._builtin_presence_cb is not None:
+            self._builtin_presence_cb(pres)
+    def _ntf_cb_builtin_accelgyro(self, sender, data):
+        d = struct.unpack("<hhhhhh", data)
+        accel_x = d[0] / 32768 * 8
+        accel_y = d[1] / 32768 * 8
+        accel_z = d[2] / 32768 * 8
+        gyro_x = d[3] / 32768 * 1000
+        gyro_y = d[4] / 32768 * 1000
+        gyro_z = d[5] / 32768 * 1000
+        if self._builtin_accelgyro_cb is not None:
+            self._builtin_accelgyro_cb((accel_x,accel_y,accel_z),(gyro_x,gyro_y,gyro_z))
+    def _ntf_cb_builtin_rgb_get(self, sender, data):
+        d = struct.unpack("<ccccH", data)
+        color = (d[0],d[1],d[2],d[3])
+        if self._builtin_rgb_transition_end_cb is not None:
+            self._builtin_rgb_transition_end_cb(color)
+            self._builtin_rgb_transition_end_cb = None
+
 
     async def gpioConfigSet(self, configs: Sequence(Tuple[int, bool, KonashiGpioPinConfig])) -> None:
         """
@@ -394,21 +459,114 @@ class Konashi:
         await self._ble_client.write_gatt_char(KONASHI_UUID_CONTROL_CMD, b)
 
 
+    async def builtinSetTemperatureCallback(self, notify_callback: Callable[[float], None]) -> None:
+        """
+        The callback is called with parameters:
+          temperature in degrees Celsius (float)
+        """
+        if notify_callback is not None:
+            self._builtin_temperature_cb = notify_callback
+            await self._ble_client.start_notify(KONASHI_UUID_BUILTIN_TEMPERATURE, self._ntf_cb_builtin_temperature)
+        else:
+            await self._ble_client.stop_notify(KONASHI_UUID_BUILTIN_TEMPERATURE)
+            self._builtin_temperature_cb = None
+
+    async def builtinSetHumidityCallback(self, notify_callback: Callable[[float], None]) -> None:
+        """
+        The callback is called with parameters:
+          humidity in percent (float)
+        """
+        if notify_callback is not None:
+            self._builtin_humidity_cb = notify_callback
+            await self._ble_client.start_notify(KONASHI_UUID_BUILTIN_HUMIDITY, self._ntf_cb_builtin_humidity)
+        else:
+            await self._ble_client.stop_notify(KONASHI_UUID_BUILTIN_HUMIDITY)
+            self._builtin_humidity_cb = None
+
+    async def builtinSetPressureCallback(self, notify_callback: Callable[[float], None]) -> None:
+        """
+        The callback is called with parameters:
+          pressure in hectopascal (float)
+        """
+        if notify_callback is not None:
+            self._builtin_pressure_cb = notify_callback
+            await self._ble_client.start_notify(KONASHI_UUID_BUILTIN_PRESSURE, self._ntf_cb_builtin_pressure)
+        else:
+            await self._ble_client.stop_notify(KONASHI_UUID_BUILTIN_PRESSURE)
+            self._builtin_pressure_cb = None
+
+    async def builtinSetPresenceCallback(self, notify_callback: Callable[[bool], None]) -> None:
+        """
+        The callback is called with parameters:
+          presence (bool)
+        """
+        if notify_callback is not None:
+            self._builtin_presence_cb = notify_callback
+            await self._ble_client.start_notify(KONASHI_UUID_BUILTIN_PRESENCE, self._ntf_cb_builtin_presence)
+        else:
+            await self._ble_client.stop_notify(KONASHI_UUID_BUILTIN_PRESENCE)
+            self._builtin_presence_cb = None
+
+    async def builtinSetAccelGyroCallback(self, notify_callback: Callable[[(float,float,float),(float,float,float)], None]) -> None:
+        """
+        The callback is called with parameters:
+          accel in g (Tuple(float,float,float))
+          gyro in degrees per second (Tuple(float,float,float))
+        """
+        if notify_callback is not None:
+            self._builtin_accelgyro_cb = notify_callback
+            await self._ble_client.start_notify(KONASHI_UUID_BUILTIN_ACCELGYRO, self._ntf_cb_builtin_accelgyro)
+        else:
+            await self._ble_client.stop_notify(KONASHI_UUID_BUILTIN_ACCELGYRO)
+            self._builtin_accelgyro_cb = None
+
+    async def builtinSetRgb(self, r: int, g: int, b: int, a: int, duration: int, transition_end_cb: Callable[[(int,int,int,int)], None] = None) -> None:
+        """
+        Set the RGB LED color.
+        r (int): Red (0~255)
+        g (int): Green (0~255)
+        b (int): Blue (0~255)
+        a (int): Alpha (0~255)
+        duration (int): duration to new color in milliseconds (0~65535)
+        """
+        b = bytearray([r&0xFF, g&0xFF, b&0xFF, a&0xFF, (duration&0x00FF), ((duration&0xFF00)>>8)])
+        await self._ble_client.write_gatt_char(KONASHI_UUID_BUILTIN_RGB_SET, b)
+        if transition_end_cb is not None:
+            self._builtin_rgb_transition_end_cb = transition_end_cb
+
+
 if __name__ == "__main__":
     async def main():
         k = Konashi(name="ksAB0FE8")
         await k.connect(5)
         print("Connected")
-        await asyncio.sleep(3)
+        await asyncio.sleep(0.5)
         def pin_change_cb(pin, val):
             print("Pin {}: {}".format(pin, val))
+        def temperature_cb(temp):
+            print("Temperature:", temp)
+        def humidity_cb(hum):
+            print("Humidity:", hum)
+        def pressure_cb(press):
+            print("Pressure:", press)
+        def presence_cb(pres):
+            print("Presence:", pres)
+        def accelgyro_cb(accel, gyro):
+            print("Accel:", accel)
+            print("Gyro:", gyro)
         k.gpioSetInputCallback(pin_change_cb)
         await k.gpioConfigSet([(0x01,True,KonashiGpioPinConfig(KONASHI_GPIO_DIRECTION_IN,send_on_change=True)), (0x1E,True,KonashiGpioPinConfig(KONASHI_GPIO_DIRECTION_OUT,send_on_change=False))])
+        await k.builtinSetTemperatureCallback(temperature_cb)
+        await k.builtinSetHumidityCallback(humidity_cb)
+        await k.builtinSetPressureCallback(pressure_cb)
+        await k.builtinSetPresenceCallback(presence_cb)
+        await k.builtinSetAccelGyroCallback(accelgyro_cb)
         await asyncio.sleep(1)
         await k.gpioControl([(0x14,KONASHI_GPIO_LEVEL_LOW), (0x0A,KONASHI_GPIO_LEVEL_HIGH)])
-        for i in range(10):
+        for i in range(40):
             await asyncio.sleep(1)
             await k.gpioControl([(0x1E,KONASHI_GPIO_LEVEL_TOGGLE)])
+            await k.builtinSetRgb(255 if i%3==0 else 0, 255 if i%3==1 else 0, 255 if i%3==2 else 0, 255, 1000)
         await asyncio.sleep(2)
         await k.disconnect()
         print("Disconnected")
